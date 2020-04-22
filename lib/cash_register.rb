34a40e9a2dec7a5aba053feb8189689a601cbc77
aromsa <annacbateman@gmail.com>
2020-04-22 13:40:09 -0400

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,27 @@
+class CashRegister
+    attr_accessor :total, :discount, :employee_discount, :add_item
+    def initialize(discount=0)
+        @total = 0
+        def employee_discount
+            @discount = 20
+        end
+        if employee_discount
+            @total = total - total * discount / 100.00
+        end
+        total
+    end
+    def add_item(title, price, quantity=1)
+        @quantity = quantity
+        @price = price * quantity
+        @total = price + total
+        # @quantity = quantity
+        # def quantity
+        #     @quantity
+        #     # @price = price * quantity
+        #     # @total = price + total
+        # end
+        total
+    end
+end
+
 
